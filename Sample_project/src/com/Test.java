6401a00a5aab4be15442972e7e216516f37954f2
Sameer-Raja7 <vasamsettisameerraja@gmail.com>
2025-02-27 16:04:27 +0530

    commit hi-feature

--- Sample_project/src/com/Test.java
+++ Sample_project/src/com/Test.java
@@ -5,5 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("I am the Manger");
 	}
-
+     public void sayHi() {
+    	 System.out.println("Hi This a developer 1");
+     }
 }
